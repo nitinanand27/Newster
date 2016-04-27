105ce88151948e623950f91453dfed1044862062
nitin anand <nitinanand27@yahoo.com>
2016-04-27 15:35:06 +0200

    added search function

--- news/news/Controllers/DefaultController.cs
+++ news/news/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using news.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,48 @@ namespace news.Controllers
     public class DefaultController : Controller
     {
         // GET: Default
-        public ActionResult Index()
-        {
-            return View();
-        }
         public ActionResult Login()
         {
             return View();
+        }
+
+        public ActionResult Index()
+        {
+            using (NewsterContext context = new NewsterContext())
+            {
+                return View(context.Articles.ToList());
+            }
+        }
+ 
+
+        public ActionResult Search()
+        {
+            NewsterContext context = new NewsterContext();
+
+            string searchString = Request["searchString"];
+
+            var articleList = context.Articles.ToList();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                var searchResult = articleList.Where(n => (n.Heading.ToLower().Contains(searchString.ToLower())) ||
+                (n.Text.ToLower().Contains(searchString.ToLower())) || (n.Categories.Where(y => y.Name.ToLower().Contains(searchString.ToLower())).Count() > 0) || (n.User.UserName.ToLower().Contains(searchString.ToLower())));
+
+                List<Article> searchArticles = new List<Article>();
+
+                foreach (var item in searchResult)
+                {
+                    searchArticles.Add(item);
+                }
+                
+                if (searchArticles.Count()!=0)
+                {
+                    return View("Index", searchArticles);
+                }
+            }
+            return RedirectToAction("Index");
+            
+
         }
     }
 }
